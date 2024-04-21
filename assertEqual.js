729201d6413125626622d7dd67979da10cfdf985
Henry <mbahenryemeka@gmail.com>
2024-04-21 12:17:20 -0400

    add emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log("\u{2705} " + "\u{2705} " + "\u{2705} " +  `Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.assert(actual === expected,  `${actual} !== ${expected}`);
+    console.log("\u{1F6D1} " + "\u{1F6D1} " + "\u{1F6D1} " + `Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
