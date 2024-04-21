009cbf6086656ba1a91311530d73fef177a6e9d0
Henry <mbahenryemeka@gmail.com>
2024-04-21 12:30:52 -0400

    use template literals for the output string

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u{2705} " + "\u{2705} " + "\u{2705} " +  `Assertion Passed: ${actual} === ${expected}`);
+    console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u{1F6D1} " + "\u{1F6D1} " + "\u{1F6D1} " + `Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`\u{1F6D1} \u{1F6D1} \u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
