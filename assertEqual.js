eb670d163309c57de126a29b6996560b2aba0e6d
Henry <mbahenryemeka@gmail.com>
2024-04-21 12:23:34 -0400

    add more conditions to test

--- assertEqual.js
+++ assertEqual.js
@@ -7,3 +7,6 @@ const assertEqual = function(actual, expected) {
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Ogam_na_south", "Ogam_na_chinko");
+assertEqual("Let's play", "Let us play");
+assertEqual(23,1925);
