b38b73df190d452175de23ae71d733f63341dc36
Henry <mbahenryemeka@gmail.com>
2024-04-26 15:06:34 -0400

    update the console.log information

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,21 +14,9 @@ const eqArrays = function (arr1, arr2){
 
 const assertArraysEqual = function (arrA, arrB){
   if (eqArrays(arrA, arrB)){  //implement function resuse. the eqArrays function can compare both the length of the arrays and the elements inside the array to ascertain whether they are equal.
-    console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: arrA === arrB`);// information to log out when arrA === arrB
+    console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${arrA} === ${arrB}`);// information to log out when arrA === arrB
   }else {
-    console.log(`\u{1F6D1} \u{1F6D1} \u{1F6D1} Assertion Failed: arrA !== arrB`); // information to log out when arrA !== arrB
+    console.log(`\u{1F6D1} \u{1F6D1} \u{1F6D1} Assertion Failed: ${arrA} !== ${arrB}`); // information to log out when arrA !== arrB
   }
 };
 
-//Two identical arrays (should pass):
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-//Two different arrays (should fail):
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-//Arrays with different lengths (should fail):
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-//Arrays with identical nested arrays (should pass):
-assertArraysEqual([[2, 3], [4]], [[2, 3], [4]]);
-//Arrays with different nested arrays (should fail):
-assertArraysEqual([[2, 3], [4]], [[2, 3], [4, 5]]);
-//Identical arrays with different data types (should fail):
-assertArraysEqual(['1', '2', '3'], [1, 2, 3]);
