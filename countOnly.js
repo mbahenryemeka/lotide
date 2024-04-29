3bcf25526d5baa8179c4a044c0048fd3a97f94d7
Henry <mbahenryemeka@gmail.com>
2024-04-28 21:18:56 -0400

    update countonly

--- countOnly.js
+++ countOnly.js
@@ -11,7 +11,6 @@ assertEqual(1, 1);
 assertEqual("Ogam_na_south", "Ogam_na_chinko");
 assertEqual("Let's play", "Let us play");
 assertEqual(23,1925);
-console.log("------------------------------------------------------------------------");
 
 
 
