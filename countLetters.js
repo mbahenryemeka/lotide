91bdce4d10940b6e3d5642a183f2415037c4ebf4
Henry <mbahenryemeka@gmail.com>
2024-04-28 22:47:52 -0400

    use for ... of loop to iterate

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,20 +6,18 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const countLetters = function (string){
-  const result = {}; 
-  let single = string.split(""); // convert the string into a separate letter in array
-  for( let i = 0; i < single.length; i++){
-    if (single[i] !== " "){  // ignore spaces inbetween in the new array
-      if(single[i]){   
-        if (result[single[i]]){   // check whether the value in the index of i is already a key in the object result
-          result[single[i]] += 1; // if it is a key, then add 1 to the value of the key
-        }else{
-          result[single[i]] = 1;  //if not a key, initialize the key with a value of 1
-        }
+const countLetters = function(string) {
+  const result = {};
+  for (let single of string) {
+    if (single !== " ") {
+      if (result[single]) {
+        result[single] += 1;
+      } else {
+        result[single] = 1;
       }
-    }   
+    }
   }
   return result;
 };
+
 
