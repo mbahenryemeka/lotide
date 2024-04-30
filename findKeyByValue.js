7421cddb9401dbb20e8a53a2e6b805cfa7dd35bf
Henry <mbahenryemeka@gmail.com>
2024-04-29 21:03:27 -0400

    use a readable variable names

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,10 +8,10 @@ const assertEqual = function(actual, expected) {
 
 
 const findKeyByValue = function(object, value) {
-  const aok = Object.keys(object);  //get the keys in array
-  for (let loop of aok) {           // iterate over the keys in array
-    if (object[loop] === value) {    // compare the value of the key with the value given
-      return loop;
+  const keys = Object.keys(object);  //get the keys in array
+  for (let key of keys) {           // iterate over the keys in array
+    if (object[key] === value) {    // compare the value of the key with the value given
+      return key;
     }
   }
   return undefined;
@@ -33,4 +33,4 @@ const myCode = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(myCode, "Learning it is the only option for me"), "third");
-assertEqual(findKeyByValue(myCode, "Programming is difficult"), undefined);
+assertEqual(findKeyByValue(myCode, "Programming is difficult"), "first");
