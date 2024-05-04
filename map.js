e9ca59721621dd24dbdb5268e9fbb4d0bc49b220
Henry <mbahenryemeka@gmail.com>
2024-05-03 23:03:28 -0400

    use assertArraysEqual to test map result

---
+++ map.js
@@ -0,0 +1,34 @@
+const eqArrays = function (arr1, arr2){
+  if (arr1.length !== arr2.length){ // condition will first test the length of the element inside the array.
+    return false; //if not equal, it will return false and stop the execution. 
+  }
+  // after the above code completes and the both arrays are equal in length, it will move to the next code below 
+  for (let i = 0; i < arr1.length; i++){  
+    if (arr1[i] !== arr2[i]){ //testing the element of both array using index with the help of the loop
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function (arrA, arrB){
+  if (eqArrays(arrA, arrB)){  //implement function resuse. the eqArrays function can compare both the length of the arrays and the elements inside the array to ascertain whether they are equal.
+    console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${arrA} === ${arrB}`);// information to log out when arrA === arrB
+  }else {
+    console.log(`\u{1F6D1} \u{1F6D1} \u{1F6D1} Assertion Failed: ${arrA} !== ${arrB}`); // information to log out when arrA !== arrB
+  }
+};
+
+const numbers = [2, 4, 6, 8];
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+assertArraysEqual((map(numbers, (num) => num * 2)), [4,8,12,16]);
+assertArraysEqual((map(numbers, (num) => num / 2)), [1,2,3,4]);
+assertArraysEqual((map(numbers, (num) => num + 5)), [7,9,11,13]);
