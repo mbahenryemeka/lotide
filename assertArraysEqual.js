aef14b3dd01cf625659e97e50ce56b408499667e
Henry <mbahenryemeka@gmail.com>
2024-05-11 23:00:50 -0400

    update require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,3 @@
-const eqArrays = function (arr1, arr2){
-  if (arr1.length !== arr2.length){ // condition will first test the length of the element inside the array.
-    return false; //if not equal, it will return false and stop the execution. 
-  }
-  // after the above code completes and the both arrays are equal in length, it will move to the next code below 
-  for (let i = 0; i < arr1.length; i++){  
-    if (arr1[i] !== arr2[i]){ //testing the element of both array using index with the help of the loop
-      return false;
-    }
-  }
-  return true;
-};
-
-
 const assertArraysEqual = function (arrA, arrB){
   if (eqArrays(arrA, arrB)){  //implement function resuse. the eqArrays function can compare both the length of the arrays and the elements inside the array to ascertain whether they are equal.
     console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${arrA} === ${arrB}`);// information to log out when arrA === arrB
@@ -20,6 +6,4 @@ const assertArraysEqual = function (arrA, arrB){
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]); // should pass
-assertArraysEqual([1,2,3], [1,2]); // should fail
-
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const eqArrays = require("../eqArrays.js");
+const assertArraysEqual = require("../assertArraysEqual.js");
+assertArraysEqual([1,2,3], [1,2,3]); // should pass
+assertArraysEqual([1,2,3], [1,2]); // should fail
+
