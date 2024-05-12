619f55b519e81f20a6090e34281cc353ccfdd1b5
Henry <mbahenryemeka@gmail.com>
2024-05-11 23:57:15 -0400

    dry up my code

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = ("../assertArraysEqual.js");
+const middle = require("../middle.js");
+
+
+assertArraysEqual(middle([1]), [2,3]); // Should fail
+assertArraysEqual(middle([1, 2]), []); // Should pass
+assertArraysEqual(middle([1, 2, 3]), [2]); // Should pass
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [5]); // Should fail
+assertArraysEqual(middle([1, 2, 3, 4]), [2, "string"]); // Should fail
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // Should pass
