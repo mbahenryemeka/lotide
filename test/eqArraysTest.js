c5b7dd4a8c0c62fe0387a2b21ad44b3ff18c68f1
Henry <mbahenryemeka@gmail.com>
2024-05-11 22:32:44 -0400

    updated eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays.js");
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1,2,3], [4,5,6]), true);
-assertEqual(eqArrays([],[3,5], true));
+assertEqual(eqArrays([4,5,6], [4,5,6]), true);
+assertEqual(eqArrays([],[], true));
