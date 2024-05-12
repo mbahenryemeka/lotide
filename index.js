eedb8ed41f0f47bf065cb6b50dba792e30785752
Henry <mbahenryemeka@gmail.com>
2024-05-12 16:55:07 -0400

    entry point for lotide

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays.js");
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findkey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
 };
+
